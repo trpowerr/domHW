c047f7efcca121a735ef8cc8d944e657a3c428ba
Mihail <mixazbubl@yandex.ru>
2020-04-24 11:47:31 +0300

    added method replaceChild

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,29 @@ function randomInteger(min, max) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
+
 function goGame() {
   const randomNum = randomInteger(0, 3);
   const arrCellId = ['one', 'two', 'three', 'four'];
   // const divEl = document.createElement('div');
 
-  // создаем новый элемент и обзываем его 
+  // создаем div и обзываем
+  const divEl = document.createElement('div');
+  divEl.setAttribute('id', 'goblin');
+
+  // создаем картинку и помещаем в div
   const imgEl = document.createElement('img');
-  imgEl.setAttribute('id', 'goblin');
   imgEl.src = 'goblin.png';
+  divEl.appendChild(imgEl);
 
   // создаем ссылку на существующий элемент который будем заменять
   const positionCell = document.getElementById(arrCellId[randomNum]);
   // console.log(positionCell, randomNum);
   const parentCell = positionCell.parentNode;
+  // console.log(parentCell);
+  parentCell.replaceChild(divEl, positionCell);
 
-
-  parentCell.insertBefore(imgEl, positionCell);
+  // parentCell.insertBefore(imgEl, positionCell);
   // parentCell.appendChild(imgEl);
 }
 
